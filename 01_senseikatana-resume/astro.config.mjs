ecbaf580e99e03482ec85aea38e8107480bc6f85
Sergio Jurado <burocracia@yandex.com>
2026-08-25 09:09:26 +0200

    chore: agregar submodulos o mejor carpetas?

--- 01_senseikatana-resume/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://senseikatana.github.io/14_cv-sergiojurado',
-  trailingSlash: 'always',
-  build: {
-    format: 'directory',
-  },
-});
